72521ee863e7c71d621a6603771add979c4c8cd7
dbort <dbort@softserveinc.com>
2016-07-01 18:18:20 +0300

    [scratchpad] - java module added

---
+++ java/src/main/java/edu/dbortnichuk/java/Main.java
@@ -0,0 +1,17 @@
+package edu.dbortnichuk.java;
+
+/**
+ * Created by dbort on 01.07.2016.
+ */
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread();
+
+        Thread t2 = new Thread();
+
+    }
+}
---
+++ java/src/main/java/edu/dbortnichuk/java/TestRunnable.java
@@ -0,0 +1,17 @@
+package edu.dbortnichuk.java;
+
+/**
+ * Created by dbort on 01.07.2016.
+ */
+public class TestRunnable implements Runnable {
+
+
+
+    public TestRunnable(){
+
+    }
+
+    public void run() {
+
+    }
+}
