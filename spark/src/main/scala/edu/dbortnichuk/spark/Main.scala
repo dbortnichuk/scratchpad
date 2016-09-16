ff77a7c8664688dc63ce6138e817b6f9b5ff5eca
dbort <dbort@softserveinc.com>
2016-09-16 15:20:35 +0300

    [spark] - some commit

--- spark/src/main/scala/edu/dbortnichuk/spark/Main.scala
+++ spark/src/main/scala/edu/dbortnichuk/spark/Main.scala
@@ -1,6 +1,11 @@
 package edu.dbortnichuk.spark
 
-import org.apache.spark.SparkContext
+import java.io.InputStream
+import java.net.URL
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.io.Source
 
 /**
   * Created by dbortnichuk on 28-May-16.
@@ -8,7 +13,20 @@ import org.apache.spark.SparkContext
 object Main {
 
   def main(args: Array[String]) {
-    //val sc = SparkContext
+
+    val sparkConf = new SparkConf()
+    sparkConf.setAppName("test")
+    sparkConf.setMaster("local[*]")
+    val sc = new SparkContext(sparkConf)
+
+    //val is: InputStream = getClass.getClassLoader.getResourceAsStream("text.txt")
+    val filePath: String = getClass.getClassLoader.getResource("text.txt").getPath
+    val lines: Iterator[String] = Source.fromFile(filePath).getLines()
+
+    val rdd = sc.parallelize(lines.toSeq)
+    rdd.filter(s => s.length < 2).foreach(println)
+
+    sc.stop()
 
   }
 
