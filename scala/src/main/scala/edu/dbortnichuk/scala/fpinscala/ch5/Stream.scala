24089d47fdaf4f934a36b72e5420a084c67016e1
Dmytro Bortnichuk <dbortnichuk@gmail.com>
2017-02-27 13:09:07 +0200

    frinscala ch5 excercises up to 5.5 completed

--- scala/src/main/scala/edu/dbortnichuk/scala/fpinscala/ch5/Stream.scala
+++ scala/src/main/scala/edu/dbortnichuk/scala/fpinscala/ch5/Stream.scala
@@ -5,6 +5,8 @@ package edu.dbortnichuk.scala.fpinscala.ch5
   */
 import Stream._
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader
+
+import scala.annotation.tailrec
 trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
@@ -32,24 +34,49 @@ trait Stream[+A] {
   def take(n: Int): Stream[A] = {
     this match {
       case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
-      case Cons(h, t) if n == 1 => cons(h(), empty)
+      case Cons(h, _) if n == 1 => cons(h(), empty)
       case _ => empty
     }
   }
 
+
   def drop(n: Int): Stream[A] = {
     this match {
       case Cons(h, t) if n > 1 => t().drop(n - 1)
-      case Cons(h, t) if n == 1 => t()
+      case Cons(_, t) if n == 1 => t()
       case _ => empty
      }
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  def takeWhile(p: A => Boolean): Stream[A] = {
+    this match {
+      case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+      case _ => empty
+    }
+  }
 
-  def forAll(p: A => Boolean): Boolean = ???
+  def forAll(p: A => Boolean): Boolean = {
+    this match {
+      case Cons(h, t) if p(h()) => t().forAll(p)
+      case Cons(h, _) if !p(h()) => false
+      case _ => true
+    }
+  }
 
-  def headOption: Option[A] = ???
+  def forAll1(p: A => Boolean): Boolean = {
+    foldRight[Boolean](true)((h, res) => p(h) && res)
+  }
+
+  def takeWhile1(p: A => Boolean): Stream[A] = {
+    foldRight[Stream[A]](empty)((h, rs) => if(p(h)) cons(h, rs) else empty)
+  }
+
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def headOption1: Option[A] = foldRight[Option[A]](None)((a, _) => Some(a))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
@@ -85,4 +112,10 @@ object MainStream extends App {
   println("toList: " + s1.toList)
   println("take: " + s1.take(2).toList)
   println("drop: " + s1.drop(2).toList)
+  println("takeWhile: " + s1.takeWhile(_ < 3).toList)
+  println("takeWhile1: " + s1.takeWhile1(_ < 3).toList)
+  println("forAll: " + s1.forAll(_ < 5))
+  println("forAll1: " + s1.forAll1(_ < 5))
+  println("headOption: " + s1.headOption)
+  println("headOption1: " + s1.headOption1)
 }
